dd038d925c9216b976889984092825156288a6f6
Sergei Filippov <5927564+Betelgeyser@users.noreply.github.com>
2018-03-26 21:30:04 +0500

    Few CUDA docs.

--- src/cuda/cublas/functions.d
+++ src/cuda/cublas/functions.d
@@ -33,6 +33,31 @@ void cublasDestroy(cublasHandle_t handle) nothrow @nogc
 	enforceCublas(cublas.cublasDestroy(handle));
 }
 
+/**
+ * This function performs the matrix-matrix multiplication:
+ *
+ * C = αop(A)op(B) + βC
+ *
+ * where α and β are scalars, and A , B and C are matrices stored in column-major format with dimensions op(A) m × k,
+ * op(B) k × n and C m × n, respectively. Also, for matrix A op(A) = A if transa == CUBLAS_OP_N A T,
+ * if transa == CUBLAS_OP_T A H, if transa == CUBLAS_OP_C and op(B) is defined similarly for matrix B.
+ *
+ * Params:
+ *     handle = handle to the cuBLAS library context.
+ *     transa = operation op(A) that is non- or (conj.) transpose.
+ *     transb = operation op(B) that is non- or (conj.) transpose.
+ *     m = number of rows of matrix op(A) and C.
+ *     n = number of columns of matrix op(B) and C.
+ *     k = number of columns of op(A) and rows of op(B).
+ *     alpha = scalar used for multiplication.
+ *     A = array of dimensions lda x k with lda>=max(1,m) if transa == CUBLAS_OP_N and lda x m with lda>=max(1,k) otherwise.
+ *     lda = leading dimension of two-dimensional array used to store the matrix A.
+ *     B = array of dimension ldb x n with ldb>=max(1,k) if transa == CUBLAS_OP_N and ldb x k with ldb>=max(1,n) otherwise.
+ *     ldb = leading dimension of two-dimensional array used to store matrix B.
+ *     beta = scalar used for multiplication. If beta==0, C does not have to be a valid input.
+ *     C = array of dimensions ldc x n with ldc>=max(1,m).
+ *     ldc = leading dimension of a two-dimensional array used to store the matrix C.
+ */
 void cublasSgemm(
 	cublasHandle_t handle,
 	cublasOperation_t transa, cublasOperation_t transb,
@@ -58,6 +83,42 @@ void cublasSgemm(
 	);
 }
 
+/**
+ * This function performs the matrix-matrix addition/transposition:
+ *
+ * C = αop(A) + βop(B)
+ *
+ * where α and β are scalars, and A, B and C are matrices stored in column-major format with dimensions op(A) m × n,
+ * op(B) m × n and C m × n, respectively. Also, for matrix A op(A) = A, if transa == CUBLAS_OP_N A T,
+ * if transa == CUBLAS_OP_T A H, if transa == CUBLAS_OP_C and op(B) is defined similarly for matrix B.
+ *
+ * The operation is out-of-place if C does not overlap A or B.
+ *
+ * The in-place mode supports the following two operations,
+ * C = α * C + βop(B)
+ * C = αop(A) + β * C
+ * For in-place mode, if C = A, ldc = lda and transa = CUBLAS_OP_N. If C = B, ldc = ldb and transb = CUBLAS_OP_N.
+ * If the user does not meet above requirements, CUBLAS_STATUS_INVALID_VALUE is returned.
+ *
+ * The operation includes the following special cases:
+ * the user can reset matrix C to zero by setting *alpha=*beta=0.
+ * the user can transpose matrix A by setting *alpha=1 and *beta=0.
+ *
+ * Params:
+ *     handle = handle to the cuBLAS library context.
+ *     transa = operation op(A) that is non- or (conj.) transpose.
+ *     transb = operation op(B) that is non- or (conj.) transpose.
+ *     m = number of rows of matrix op(A) and C.
+ *     n = number of columns of matrix op(B) and C.
+ *     alpha = scalar used for multiplication. If *alpha == 0, A does not have to be a valid input.
+ *     A = array of dimensions lda x n with lda>=max(1,m) if transa == CUBLAS_OP_N and lda x m with lda>=max(1,n) otherwise.
+ *     lda = leading dimension of two-dimensional array used to store the matrix A.
+ *     B = array of dimension ldb x n with ldb>=max(1,m) if transa == CUBLAS_OP_N and ldb x m with ldb>=max(1,n) otherwise.
+ *     ldb = leading dimension of two-dimensional array used to store matrix B.
+ *     beta = scalar used for multiplication. If *beta == 0, B does not have to be a valid input.
+ *     C = array of dimensions ldc x n with ldc>=max(1,m).
+ *     ldc = leading dimension of a two-dimensional array used to store the matrix C.
+ */
 void cublasSgeam(
 	cublasHandle_t handle,
 	cublasOperation_t transa, cublasOperation_t transb,
