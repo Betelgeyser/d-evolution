d96a2c60971b716bbe950491e2845fdb83c7a2ff
Sergei Filippov <5927564+Betelgeyser@users.noreply.github.com>
2018-06-13 13:57:50 +0500

    First tests. Ugly, but works

--- src/app.d
+++ src/app.d
@@ -14,64 +14,202 @@
  * limitations under the License.
  */
 
-import std.stdio;
-import std.random : Mt19937_64, unpredictableSeed;
-import std.random    : uniform;
-import std.range     : generate, take;
-import std.csv;
-import std.file;
-import std.array;
-import std.algorithm;
-import std.typecons;
-import std.datetime;
+// Standard D modules
+import core.time              : Duration, minutes, seconds;
+import std.algorithm          : count;
+import std.conv               : to;
+import std.csv                : csvReader;
+import std.datetime.stopwatch : StopWatch;
+import std.file               : readText;
+import std.range              : zip;
+import std.stdio              : stdout, write, writeln;
+import std.string             : format;
+import std.typecons           : tuple;
 
-import dnn.nn.network;
-import dnn.ga.genome;
-import dnn.ga.population;
+// Cuda modules
+import cuda.cudaruntimeapi;
+import cuda.cublas;
+import cuda.curand;
+
+// DNN modules
+import common               : ANSIColor, ansiFormat, humanReadable;
+import evolution.population : NetworkParams, Population;
+import math.matrix          : Matrix, transpose;
+import math.random          : RandomPool;
+import math.statistics      : MAE, MASE;
+
 
 void main()
 {
-	SpecimenParams sp;
-	sp.inputs  = 1;
-	sp.outputs = 1;
-	sp.layers  = 3;
-	sp.neurons = 3;
-	sp.weights.min = -1_000_000_000;
-	sp.weights.max =  1_000_000_000;
+	StopWatch stopWatch;
 	
-	Population!Network population;
+	uint[]  inputs  = [30, 7, 1];//, 90];
+	uint[]  outputs = [1];//, 7];//, 30];
+	uint[]  layers  = [2];//,  3];//,  4,  5,  6,  7, 8, 9, 10, 11, 12];
+	uint[]  neurons = [2, 4, 6, 8, 10];//, 15, 20];
+	float[] min     = [-1.0e3, -1.0e6, -1.0e9];
+	float[] max     = [ 1.0e3,  1.0e6,  1.0e9];
 	
-	auto file = File("tests.csv", "r");
+	uint[] populationMultiplier = [10];//[1, 5, 10];//, 15, 20];
 	
-	double[][] inputData;
-	double[][] outputData;
+	auto curandGenerator = CurandGenerator(curandRngType_t.PSEUDO_DEFAULT);
+	scope(exit) curandGenerator.destroy;
 	
-	foreach (row; file.byLine.joiner("\n").csvReader!(Tuple!(double, double, double, double, double)))
-	{
-		inputData  ~= [ row[0] ];
-		outputData ~= [ row[4] ];
-	}
+	auto pool = RandomPool(curandGenerator);
+	scope(exit) pool.freeMem();
 	
-	population.loadData(inputData, outputData);
-	population.specimenParams = sp;
+	cublasHandle_t cublasHandle;
+	cublasCreate(cublasHandle);
+	scope(exit) cublasDestroy(cublasHandle);
 	
-	StopWatch sw;
-	sw.start();
-	population.populate(1000);
-	writeln("Initial population:");
-	writeln(">>> Best fitness = ", population.bestFitness, "; avg fitness = ", population.avgFitness);
+	ulong trialNumber = 0;
 	
-	writeln(sw.peek().msecs(), " msec");
+	auto timeLimit       = 15.minutes();
+//	auto timeLimit       = 5.seconds();
+	uint generationLimit = 5000;
 	
-	ulong counter;
-	do
-	{
-		counter++;
-		population.selection();
-		writeln("Generation ", counter, ":");
-		writeln(">>> Best = ", population.bestFitness, "; worst = ", population.worstFitness, "; avg = ", population.avgFitness);
-	
-		writeln(sw.peek().msecs(), " msec");
-	} while (population.bestFitness > 0.01 && counter < 10_000);
-	sw.stop();
+	foreach (i; inputs)
+		foreach (o; outputs)
+			foreach (l; layers)
+				foreach (n; neurons)
+					foreach (mul; populationMultiplier)
+						foreach (boundaries; zip(min, max))
+						{
+							writeln("Setting up trial #%d".format(++trialNumber).ansiFormat(ANSIColor.white));
+							
+							NetworkParams params = {
+								inputs  : i * 7,
+								neurons : n,
+								outputs : o * 4,
+								layers  : l,
+								min     : boundaries[0],
+								max     : boundaries[1]
+							};
+							
+							writeln(
+								("\tNetwork parameters: "
+									~ "%d".ansiFormat(ANSIColor.white) ~ " inputs, "
+									~ "%d".ansiFormat(ANSIColor.white) ~ " hidden neurons, "
+									~ "%d".ansiFormat(ANSIColor.white) ~ " outputs, "
+									~ "%d".ansiFormat(ANSIColor.white) ~ " layers, "
+									~ "minimum weigth is " ~ "%f ".ansiFormat(ANSIColor.white)
+									~ "maximun weight is " ~ "%f. ".ansiFormat(ANSIColor.white)
+									~ "In total %d degrees of freedom"
+								).format(
+									params.inputs,
+									params.neurons,
+									params.outputs,
+									params.layers,
+									params.min,
+									params.max,
+									params.degreesOfFreedom
+							));
+							
+							writeln(
+								("\tPopulation multiplier = %d. Total population size is "
+									~ "%d".ansiFormat(ANSIColor.white) ~ " networks"
+								).format(mul, mul * params.degreesOfFreedom)
+							);
+							
+							write("\tGenerating population...");
+							stdout.flush();
+							
+							auto population = Population(params, params.degreesOfFreedom * mul, pool);
+							scope(exit) population.freeMem();
+							
+							writeln(" [ " ~ "done".ansiFormat(ANSIColor.green) ~ " ]");
+							writeln("\tEstimated memory usage: " ~ population.size.humanReadable.ansiFormat(ANSIColor.white));
+							
+							auto trainingInputs = Matrix(readText("data/" ~ i.to!string ~ "/training/inputs.csv"));
+							scope(exit) trainingInputs.freeMem();
+							
+							auto trainingOutputs = Matrix(readText("data/" ~ i.to!string ~ "/training/outputs.csv"));
+							scope(exit) trainingOutputs.freeMem();
+							
+							auto testingInputs = Matrix(readText("data/" ~ i.to!string ~ "/testing/inputs.csv"));
+							scope(exit) testingInputs.freeMem();
+							
+							auto testingOutputs = Matrix(readText("data/" ~ i.to!string ~ "/testing/outputs.csv"));
+							scope(exit) testingOutputs.freeMem();
+							
+							writeln(
+								("\tGeneration is capped at " ~ "%d".ansiFormat(ANSIColor.white)
+									~ ", time limit is set to " ~ "%s".ansiFormat(ANSIColor.white)
+								).format(generationLimit, timeLimit)
+							);
+							
+							stopWatch.start();
+							while (true)
+							{
+								write("\tGeneration #%d: ".format(population.generation).ansiFormat(ANSIColor.white));
+								stdout.flush();
+								
+								population.fitness(trainingInputs, trainingOutputs, cublasHandle);
+								
+								writeln(
+									("\tbest = " ~ "%f".ansiFormat(ANSIColor.brightGreen) ~
+										"\tworst = " ~ "%f".ansiFormat(ANSIColor.brightRed) ~
+										"\tmean = " ~ "%f".ansiFormat(ANSIColor.brightYellow)
+									).format(
+										population.best.fitness,
+										population.worst,
+										population.mean
+								));
+								
+								if (stopWatch.peek() >= timeLimit)
+								{
+									writeln("\tExceeded time limit [ " ~ "stopped".ansiFormat(ANSIColor.red) ~ " ]");
+									break;
+								}
+								
+								if (population.generation >= generationLimit)
+								{
+									writeln("\tGenerations cap reached [ " ~ "stopped".ansiFormat(ANSIColor.red) ~ " ]");
+									break;
+								}
+								
+								if (population.best.fitness <= 0.05)
+								{
+									writeln("\tAcceptable solution found [ " ~ "stopped".ansiFormat(ANSIColor.green) ~ " ]");
+									break;
+								}
+								
+								population.evolve(pool);
+							}
+							stopWatch.stop();
+							stopWatch.reset();
+							
+							// TODO: Check validation data
+							auto trainingOutputsT = Matrix(trainingOutputs.cols, trainingOutputs.rows);
+							scope(exit) trainingOutputsT.freeMem();
+							
+							auto approx = Matrix(trainingOutputs.rows, trainingOutputs.cols);
+							scope(exit) approx.freeMem();
+							
+							auto approxT = Matrix(trainingOutputs.cols, trainingOutputs.rows); // MASE operates on transposed matrices
+							scope(exit) approxT.freeMem();
+							
+							transpose(trainingOutputs, trainingOutputsT, cublasHandle);
+							
+							population.best()(trainingInputs, approx, cublasHandle);
+							
+							transpose(approx, approxT, cublasHandle);
+							
+							float mase = MASE(trainingOutputsT, approxT, cublasHandle);
+							float mae = MAE(trainingOutputsT, approxT, cublasHandle);
+							
+							cudaDeviceSynchronize();
+							
+							foreach (j, v; approx)
+								writeln("\t>>> Expected %f, got %f".format(trainingOutputs[j], approx[j]));
+							
+							writeln(
+								("\tTrial results: ".ansiFormat(ANSIColor.white) ~
+									"best MASE = " ~ "%f".ansiFormat(ANSIColor.brightGreen) ~
+									", best MAE = " ~ "%f".ansiFormat(ANSIColor.brightGreen)
+								).format(mase, mae)
+							);
+							writeln("Best solution so far is ", population.best());
+						}
 }
+
