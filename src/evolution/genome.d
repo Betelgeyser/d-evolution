6bbd1849aad8e6bd200e28f03865bae10b967fba
Sergei Filippov <5927564+Betelgeyser@users.noreply.github.com>
2018-11-28 11:43:28 +0500

    Deprecating genome module

--- src/evolution/genome.d
+++ src/evolution/genome.d
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-module evolution.genome;
+deprecated module evolution.genome;
 
 import core.stdc.stdlib;
 
