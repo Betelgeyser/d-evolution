db581a74f82ede99663e784677e38676762ccf6e
Sergei Filippov <5927564+Betelgeyser@users.noreply.github.com>
2018-11-28 13:00:07 +0500

    Memory manager optimisation in progress

--- src/memory.d
+++ src/memory.d
@@ -17,11 +17,17 @@
  */
 module memory; // TODO: All contents of this module are non thread-safe!
 
+/*
+ * TODO: Use associative arrays (hashmaps) to free block at O(n) and then append them list of free blocks.
+ * One more option is to use self balancing binary search tree for free blocks to speed up searching of a sufficient free block.
+ */
+
 // Standard D modules
 import core.stdc.stdlib : free, malloc;
 import std.conv         : to;
 import std.string       : format;
 import std.traits       : isNumeric;
+import std.typecons     : Nullable, nullable, Tuple, tuple;
 
 import common;
 
@@ -372,10 +378,12 @@ struct Block
 	@disable this();
 	
 	/**
+	 * Allocate a new block of a size $(D_PARAM size).
+	 *
+	 * This constructor does actual memory allocation.
+	 *
 	 * Params:
-	 *     ptr = Root pointer of the block.
 	 *     size = Size of the block in bytes.
-	 *     isAllocated = Has block already been allocated?
 	 */
 	this(in size_t size) @nogc nothrow
 	{
@@ -384,19 +392,43 @@ struct Block
 		_isAllocated = false;
 	}
 	
-	/// ditto
-	this(void* ptr, in size_t size, in bool isAllocated) @nogc nothrow pure
+	/**
+	 * Create a new block.
+	 *
+	 * Params:
+	 *     ptr = Pointer to the block.
+	 *     size = Size of the new block.
+	 *     isAllocated = Has this block already been allocated or not.
+	 */
+	this(void* ptr, in size_t size, in bool isAllocated = false) @nogc nothrow pure
 	{
 		_ptr         = ptr;
 		_size        = size;
 		_isAllocated = isAllocated;
 	}
 	
+	this(void* ptr, in size_t size, in bool isAllocated = false, void* prev = null, void* next = null) @nogc nothrow pure @safe
+	{
+		_ptr         = ptr;
+		_size        = size;
+		_isAllocated = isAllocated;
+		_prev        = prev;
+		_next        = next;
+	}
+	
 	private
 	{
 		void*  _ptr;         /// Pointer to the block's memory.
 		size_t _size;        /// Size of the block.
-		bool   _isAllocated; /// $(D_KEYWORD true) if the block has been allocated.
+		bool   _isAllocated; /// $(D_KEYWORD true) if the block is allocated.
+		void*  _prev = null; /// Pointer to the previous adjucent block.
+		void*  _next = null; /// Pointer to the next adjucent block.
+	}
+	
+	invariant
+	{
+		assert (_size > 0);
+		assert (_ptr !is null);
 	}
 	
 	/**
@@ -416,11 +448,35 @@ struct Block
 	}
 	
 	/**
+	 * Returns: Pointer of (not 'to') the previous adjucent block.
+	 */
+	@property void* prev() @nogc nothrow pure @safe
+	{
+		return _prev;
+	}
+	
+	/**
+	 * Returns: Pointer of (not 'to') the next adjucent block.
+	 */
+	@property void* next() @nogc nothrow pure @safe
+	{
+		return _next;
+	}
+	
+	/**
 	 * Returns: $(D_KEYWORD true) if the block is free.
 	 */
 	@property bool isFree() const @nogc nothrow pure @safe
 	{
 		return !_isAllocated;
+	}
+	
+	/**
+	 * Returns: $(D_KEYWORD true) if the block is allocated.
+	 */
+	@property bool isAllocated() const @nogc nothrow pure @safe
+	{
+		return _isAllocated;
 	}
 	
 	/**
@@ -456,7 +512,101 @@ struct Block
 	void free() @nogc nothrow pure @safe
 	{
 		_isAllocated = false;
-		debug(memory) writeLog(_ptr, " is freed");
+	}
+	
+	/**
+	 * Split a block into two blocks.
+	 *
+	 * Although this function splits the block into two blocks, if $(D_PARAM size) is set to 0 or equals to the block size,
+	 * only one block is returned, that is $(D_KEYWORD this) block itself.
+	 *
+	 * This function is useful as it automatically manages `_prev` and `_next` pointers of splited blocks.
+	 *
+	 * Params:
+	 *     Size = Size of the left block.
+	 *
+	 * Returns:
+	 *     Array of one or two blocks. The firts one is of $(D_PARAM size) size, the second one is of the remaining size.
+	 */
+	Block[] split(in size_t size) nothrow pure
+	out(result)
+	{
+		assert (result.length == 1 || result.length == 2);
+	}
+	body
+	{
+		if (size > _size)
+			throw new Error("Block split into %d bytes, while only %d is avaliable.".format(size, _size));
+		
+		if (size == 0)
+			return [this];
+		
+		if (size < _size)
+		{
+			void*  nextPtr  = _ptr + size;
+			size_t nextSize = _size - size;
+			
+			return [
+				Block(_ptr,    size,     _isAllocated, _prev, nextPtr),
+				Block(nextPtr, nextSize, _isAllocated, _ptr,  _next)
+			];
+		}
+		
+		return [this];
+	}
+	
+	///
+	unittest
+	{
+		mixin(writeTest!split);
+		
+		auto block = Block(100);
+		
+		assert (block.split(0).length          == 1);
+		assert (block.split(block.size).length == 1);
+		
+		assert (block.split(0)[0]          == block);
+		assert (block.split(block.size)[0] == block);
+		
+		immutable splitSize = 40;
+		auto splitBlocks = block.split(splitSize);
+		
+		assert (splitBlocks.length == 2);
+		
+		assert (splitBlocks[0].ptr == block.ptr);
+		assert (splitBlocks[1].ptr == block.ptr + splitSize);
+		
+		assert (splitBlocks[0].size == splitSize);
+		assert (splitBlocks[1].size == block.size - splitSize);
+		
+		assert (splitBlocks[0].prev is null);
+		assert (splitBlocks[1].prev == splitBlocks[0].ptr);
+		
+		assert (splitBlocks[0].next == splitBlocks[1].ptr);
+		assert (splitBlocks[1].next is null);
+	}
+	
+	/// TODO: Error control.
+	static Block merge(Block leftBlock, Block rightBlock)
+	{
+		return Block(
+			leftBlock.ptr,
+			leftBlock.size + rightBlock.size,
+			leftBlock.isAllocated,
+			leftBlock.prev,
+			rightBlock.next
+		);
+	}
+	
+	///
+	unittest
+	{
+		mixin(writeTest!merge);
+		
+		auto block = Block(100);
+		auto splitBlocks = block.split(60);
+		
+		assert (merge(splitBlocks[0], splitBlocks[1]) == block);
 	}
 }
 
@@ -597,6 +747,8 @@ struct UnifiedMemoryManager
 	private
 	{
 		List!Pool _pools; /// List of avaliable pools.
+		Block[void*] _allocatedBlocks; /// AA of all existing memory blocks.
+		Block[void*]      _freeBlocks; /// AA of free memory blocks.
 	}
 	
 	/**
@@ -644,6 +796,66 @@ struct UnifiedMemoryManager
 			debug(memory) ++i;
 		}
 		debug(memory) assert (false, "%s %d\tUnable to free %s, memory violation".format(__FILE__, __LINE__, array.ptr));
+	}
+	
+	T[] allocate2(T)(in size_t items) nothrow
+		if (isNumeric!T)
+	{
+		if (items == 0)
+			return null;
+		
+		size_t size = items * T.sizeof;
+		
+		foreach (freeBlock; _freeBlocks.byValue())
+		{
+			if (freeBlock.size >= size)
+			{
+				auto newBlocks = freeBlock.split(size);
+				
+				_allocatedBlocks[newBlocks[0].ptr] = newBlocks[0];
+				_allicatedBlocks[newBlocks[0].ptr].allocate();
+				
+				if (newBlocks.lenght == 2)
+					_freeBlocks[newBlocks[1].ptr] = newBlocks[1].ptr;
+				
+				_freeBlocks.remove(freeBlock.ptr);
+				
+				return cast(T[])newBlocks[0].ptr[0 .. size];
+			}
+		}
+		
+		return null;
+	}
+	
+	void free2(T)(ref T[] array) nothrow
+	{
+		auto block = _allocatedBlocks[array.ptr];
+		
+		if (block.prev in _freeBlocks)
+		{
+			auto prevBlock = _freeBlocks[block.prev];
+			
+			newPtr = prevBlock.ptr;
+			newSize += prevBlock.size;
+			newPrev = prevBlock.prev;
+			
+			_freeBlocks.remove(block.prev);
+		}
+		
+		if (block.next in _freeBlocks)
+		{
+			auto nextBlock = _freeBlocks[block.next];
+			
+			newSize += nextBlock.size;
+			newNext = nextBlock.next;
+			
+			_freeBlocks.remove(block.next);
+		}
+		
+		_freeBlocks[newPtr] = Block(newPtr, newSize, false, newPrev, newNext);
+		_allocatedBlocks.remove(block.ptr);
+		
+		array = null;
 	}
 	
 	private
